43de355ae029fbf04843af095fc91031f4d22526
Kristie Nguyen <nguyenkristie@live.com>
2014-10-17 14:44:04 -0700

    added project 5

---
+++ Project05-Dialog/HelloWorldDialog.java
@@ -0,0 +1,11 @@
+// Created by Kristie Nguyen on 10/10/2014.
+
+import javax.swing.JOptionPane;
+
+public class HelloWorldDialog 
+{
+	public static void main( String[] args )
+	{
+		JOptionPane.showMessageDialog( null, "Hello world!" );
+	}
+}
