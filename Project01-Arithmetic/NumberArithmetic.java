9268c87ab4535f3001833b6b5d6fe2c5328f697a
Kristie Nguyen <nguyenkristie@live.com>
2014-10-17 14:42:48 -0700

    added project 1

---
+++ Project01-Arithmetic/NumberArithmetic.java
@@ -0,0 +1,38 @@
+// Created by Kristie Nguyen on 9/16/2014.
+// TablesOfSquaresAndCubes - Calculates and prints the squares
+// and cubes of numbers 0 to 10.
+
+public class NumberArithmetic
+{
+    public static void main( String[] args )
+    {
+    	// initialize
+        int x = 0;
+
+        // process and output
+        System.out.printf( "%s\t%s\t%s\n", "Number", "Squared", "Cubed" );
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+        System.out.printf( "%d\t%d\t%d\n", x, ( x * x ), ( x * x * x ) );
+        x++;
+    }
+}
+
