f5817b41594765483ec2802abd3898172170edbd
Kristie Nguyen <nguyenkristie@live.com>
2014-10-18 21:49:53 -0700

    added toString class

--- Project07-Arrays/StringArray.java
+++ Project07-Arrays/StringArray.java
@@ -1,4 +1,35 @@
+// Created by Kristie Nguyen on 10/18/2014.
+// StringArray - Use a loop to put strings into an array and outputs the strings.
 
-public class StringArray {
+public class StringArray 
+{
+	private String suits;
+	
+	public StringArray( String suitsString )
+	{
+		suits = suitsString;
+	}
+	
+	public String toString()
+	{
+		return suits;
+	}
 
+	public double setArray()
+	{
+		String temp = "";
+		
+		for ( int counter = 0; counter < String.length; counter++ )
+		{
+			temp = "Hearts" + counter;
+			String[counter] = temp;
+		}
+	}
+	
+	public void displayArray()
+	{
+		String[] stringArray = new String[ 4 ];
+		for ( int counter = 0; counter < stringArray.length; counter++ )
+		System.out.printf("The strings in the array are: %s", stringArray[ counter ] );
+	}
 }
