e21572fecbf564105c6df2165cbdc77e9b82e89f
Kristie Nguyen <nguyenkristie@live.com>
2014-10-18 18:35:26 -0700

    deleted test, added getLength method

--- Project07-Arrays/LengthOfVector.java
+++ Project07-Arrays/LengthOfVector.java
@@ -1,25 +1,30 @@
-// Created by Kristie Nguyen on 10/14/2014.
-// LengthOfVector - Finds the length of a vector.
-// The length of a vector is the square root of 
-// the sum of the squares of the elements in the array.
+// Created by Kristie Nguyen on 10/18/2014.
+// LengthOfVector - A function that finds the length of a vector.
+// The length of a vector is the square root of
+// the sum of the squares of the elements in a given array.
 
-public class LengthOfVector 
+public class LengthOfVector
 {
-	public void main( String[] args )
+	public static void main( String[] args )
 	{
-	public VectorLength()
-	{
-		double length = (double[] elements)
-		{
-			double sum = 0;
+		// input for array
+		double[] arrayX = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		
-			for ( int count = 0; count < elements.length; count++)
-			{
-				sum = sum + elements[ count ] * elements[ count ];
-			}
-		
-			return Math.sqrt( sum );
-		}
+		// output vector lengths.
+		System.out.println("The length of the vectors are:");
+		getLength( arrayX );
+		for ( double length : arrayX )
+			System.out.printf("%.1f\n", length );
 	}
-}
+	
+	public static double getLength( double[] arrayX2 )
+	{
+		double sum = 0;
+		
+		for (int counter = 0; counter < arrayX2.length; counter++)
+		{
+			sum = sum + ( arrayX2[ counter ] * arrayX2[ counter ] );
+		}
+		return Math.sqrt( sum );
+	}
 }
