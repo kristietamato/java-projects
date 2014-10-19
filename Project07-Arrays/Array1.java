054e5dac85f9c78edba2ac4b46ca5b45654d45b8
Kristie Nguyen <nguyenkristie@live.com>
2014-10-18 17:30:55 -0700

    added working array1 class

--- Project07-Arrays/Array1.java
+++ Project07-Arrays/Array1.java
@@ -1,5 +1,5 @@
 // Created by Kristie Nguyen on 10/17/2014.
-// A for loop for an array.
+// Array1 - find values of an array given another array.
 
 public class Array1
 {
@@ -8,8 +8,10 @@ public class Array1
 		int[] arrayX = new int[ 3 ];
 		int[] arrayY = { 1, 2, 3 };
 
-		for ( int i = 0; i < 3; i++ )
-			arrayX[ i ] = arrayY[ 2 - i ];
-		System.out.printf("%d", arrayX);
+		for ( int counter = 0; counter < 3; counter++ )
+		{
+			arrayX[ counter ] = arrayY[ 2 - counter ];
+			System.out.printf("%d ", arrayX[counter]);
+		}
 	}
 }
