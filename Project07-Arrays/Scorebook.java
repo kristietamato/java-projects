e85f0bb49e8df3380652199b4a26aecf2e84a556
Kristie Nguyen <nguyenkristie@live.com>
2014-10-18 20:43:06 -0700

    added working scorebool class

---
+++ Project07-Arrays/Scorebook.java
@@ -0,0 +1,27 @@
+// Created by Kristie Nguyen on 10/18/2014.
+// Average - Gets the average of an array of scores.
+public class Scorebook 
+{
+	private double[] scores;
+	
+	public Scorebook( double[] scoresArray )
+	{
+		scores = scoresArray;
+	}
+
+	public double getAverage()
+	{
+		double total = 0;
+		
+		for ( double score : scores )
+		{
+			total = total + score;
+		}
+		return ( total / scores.length );
+	}
+	
+	public void displayAverage()
+	{
+		System.out.printf("The average score is: %.2f", getAverage() );
+	}
+}
---
+++ Project07-Arrays/ScorebookTest.java
@@ -0,0 +1,14 @@
+// Created by Kristie Nguyen on 10/18/2014.
+// AverageTest - main class to test Average class.
+
+public class ScorebookTest 
+{
+	public static void main( String[] args )
+	{
+		double[] scores = { 7.572, 8.066, 8.186, 7.652, 41.860 };
+		
+		Scorebook myScorebook = new Scorebook( scores );
+		myScorebook.getAverage();
+		myScorebook.displayAverage();
+	}
+}
