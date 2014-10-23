f477645c37db2291aa7c8b5f8ad04cb41b878808
Kristie Nguyen <nguyenkristie@live.com>
2014-10-22 19:25:31 -0700

    created layout

--- Project08-Rectangle/Rectangle.java
+++ Project08-Rectangle/Rectangle.java
@@ -1,4 +1,25 @@
+// Created by Kristie Nguyen on 10/22/14.
+// Rectangle - Calculates a rectangle's perimeter and area; has set methods
+// that verify that length and width are each floating-point numbers 
+// larger than 0.0 and less than 20.0.
+public class Rectangle
+{
+	// variables
+	float length;
+	float width;
 
-public class Rectangle {
+	public Rectangle()
+	{
+		// constructor
+		length = 1;
+		width = 1;
+	}
 
+	// set length
+
+	// set width
+
+	// get perimeter
+
+	// get area
 }
